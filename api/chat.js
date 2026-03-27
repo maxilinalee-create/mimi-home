027641717a3dac5d98dcfcf5009a1c95c56a950d
maxilinalee-create <maxilina.lee@gmail.com>
2026-03-27 16:59:04 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
     const API_KEY = keys[Math.floor(Math.random() * keys.length)];
 
     try {
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {
+        const response = await fetch(https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY} `, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
