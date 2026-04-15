ecc739bd20b1adcb3f52dcb3a63ccbc16377f7e3
maxilinalee-create <maxilina.lee@gmail.com>
2026-04-15 14:54:06 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,5 @@
-export const config = { api: { bodyParser: { sizeLimit: '10mb' } } 
+export const config = {
+  api: { bodyParser: { sizeLimit: '10mb' } }
 };
 
 export default async function handler(req, res) {
